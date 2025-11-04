54abf069cbcc39d3c4a3bc9b23ae906cac824be3
clarice <cqas@discente.ifpe.edu.br>
2025-11-04 08:44:18 -0300

    Prática 3 - Parte 3 Adicionando elementos com um Diálogo.

--- app/src/main/java/com/example/WeatherApp/MainActivity.kt
+++ app/src/main/java/com/example/WeatherApp/MainActivity.kt
@@ -17,9 +17,14 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.example.WeatherApp.model.MainViewModel
+import com.example.WeatherApp.ui.CityDialog
 import com.example.WeatherApp.ui.nav.BottomNavBar
 import com.example.WeatherApp.ui.nav.BottomNavItem
 import com.example.WeatherApp.ui.nav.MainNavHost
@@ -31,9 +36,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            var showDialog by remember { mutableStateOf(false) }
             val viewModel : MainViewModel by viewModels()
             val navController = rememberNavController()
             WeatherAppTheme {
+                if (showDialog) CityDialog(
+                    onDismiss = { showDialog = false },
+                    onConfirm = { city ->
+                        if (city.isNotBlank()) viewModel.add(city)
+                        showDialog = false
+                    })
                 Scaffold(
                     topBar = {
                         TopAppBar(
@@ -66,7 +78,7 @@ class MainActivity : ComponentActivity() {
 
                     floatingActionButton = {
 
-                        FloatingActionButton(onClick = { }) {
+                        FloatingActionButton(onClick = { showDialog = true}) {
                             Icon(Icons.Default.Add, contentDescription = "Adicionar")
                         }
                     }
