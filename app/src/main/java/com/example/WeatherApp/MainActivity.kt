76b9b08e271fe5133aaa71fe76fb4e21411dba89
clarice <cqas@discente.ifpe.edu.br>
2025-11-18 09:46:56 -0300

    Prática 5 -Parte 3: Realizando Monitorando o Status da Sessão do Usuário

--- app/src/main/java/com/example/WeatherApp/MainActivity.kt
+++ app/src/main/java/com/example/WeatherApp/MainActivity.kt
@@ -67,7 +67,7 @@ class MainActivity : ComponentActivity() {
                             actions = {
 
                                 IconButton( onClick = { Firebase.auth.signOut()
-                                    finish() } ) {
+                                    } ) {
                                     Icon(
                                         imageVector =
                                             Icons.AutoMirrored.Filled.ExitToApp,
